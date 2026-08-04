31e00398b6fb5c06697bc748f477fe51969e65c0
Lee JaeHyun <myleje@gmail.com>
2026-08-04 19:20:28 +0900

    feat: Project 초기 설정

---
+++ GenericStudy/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericStudy;
+
+/*
+ * 제너릭(Generic)
+ * 1. 코드의 재사용성
+ * 2. 타입의 안정성
+ * 3. 성능 향상 : object Boxing / Unboxing 
+ */
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    
+    // static int AddInt(int a, int b) => a + b;
+    // static float AddFloat(float a, float b) => a + b;
+}
+
